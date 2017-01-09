7adf7cb29a32cd1d8f330fbf28196dd706dcf244
Nicola Zanardi <nicola.zanardi@gmail.com>
2017-01-09 11:52:15 -0500

    Finished section nowadays powertrain trends

--- auto/chapter2.el
+++ auto/chapter2.el
@@ -1,5 +1,5 @@
 (TeX-add-style-hook
- "chapter1"
+ "chapter2"
  (lambda ()
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
@@ -9,10 +9,13 @@
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
-    "sec:global_warming"
-    "antropogenic_ghg_emissions"
-    "fig:mitigationscenarios"
-    "fig:radiative_forcing"
-    "fig:average_fuel_efficiency"))
+    "fig:adj_fuel_economy"
+    "fig:improvement_valve_fuel_delivery"
+    "fig:technology_trends"
+    "sec:technology_improvements"
+    "fig:emission_standards"
+    "fig:emission_levels"
+    "fig:turbodownsizing_distribution"
+    "fig:hybrids"))
  :latex)
 
--- auto/thesis.el
+++ auto/thesis.el
@@ -5,22 +5,17 @@
                      '(("report" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("geometry" "letterpaper" "left=1.5in" "right=1in" "top=1in" "bottom=1in") ("titlesec" "explicit") ("tocloft" "titles") ("biblatex" "backend=bibtex" "sorting=none" "bibstyle=ieee") ("hyperref" "bookmarks=true" "hidelinks") ("appendix" "page")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
-    "titlePage"
-    "approvalPage"
-    "epigraph"
-    "dedication"
-    "acknowledgements"
-    "summary"
     "chapter1"
+    "chapter2"
     "report"
     "rep12"
     "graphicx"
